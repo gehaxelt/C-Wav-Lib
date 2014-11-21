57f1e950310ee28563d727078b5a5fcaa465b969
Sebastian Gehaxelt <github@gehaxelt.in>
2014-11-21 22:54:03 +0100

    Create random songs from /dev/urandom

--- main.c
+++ main.c
@@ -10,6 +10,8 @@ int main() {
     char *file2 = "test2.wav";
 
     FILE *f = fopen(file2,"wb");
+    FILE *random = fopen("/dev/urandom","r");
+
     unsigned int test_channels = 2;
     unsigned int test_sampleRate = 44000;
     unsigned int test_sampleByte = 2;
@@ -33,16 +35,19 @@ int main() {
 
     int i,j;
     double high,low;
+    unsigned short note;
     for(i=0;i<test_samplesPerChannel*test_seconds;i++) {
+        if(i % (test_samplesPerChannel/(i % 5 + 2)) == 0) {
+            note = 0;
+            fread(&note, sizeof(note),1,random);
+            note = note % 600 + 50;
+            printf("Hertz: %d\n",note);
+
+        }
         for(j=0;j<test_channels;j++) {
-            //                                             sin(2*Pi*(Hz / 44000)*i)
-            high = sin( (double) i * ( ((double) (test_hertz + ((double)(i % 1000))/440)  * 2 * M_PI) / (double)test_sampleRate));
-            if((i%(22000))<1100*test_seconds) {
-                low = sin( (double) i * ( ((double) (test_hertz + (50)  * 2 * M_PI) / (double)test_sampleRate)));
-            } else {
-                low = sin( (double) i * ( ((double) (test_hertz + (75)  * 2 * M_PI) / (double)test_sampleRate)));
-            }
-            *(((short *)sampleData) + j) = to_le16((short)(( high/2 + low*4 )*0xFFF));
+            test_hertz = (unsigned int) note;
+            low = sin( (double) i * ( ((double) (test_hertz * 2 * M_PI) / (double)test_sampleRate)));
+            *(((short *)sampleData) + j) = to_le16((short)(( low*4 )*0xFFF));
         }
         sample.sampleData = sampleData;
         write_wav_sample(f,&sample);
@@ -50,7 +55,7 @@ int main() {
 
     free(sampleData);
     fclose(f);
-
+    fclose(random);
     wav_header_t *readHeader = malloc(sizeof(wav_header_t));
 
     if(readHeader==NULL) {
